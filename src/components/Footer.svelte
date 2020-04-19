5dc7074d47a789c8748bf5b3a08852ff9f4ba04d
Esdras Pavon <esdras.pavon@devlights.com>
2020-04-19 13:31:55 -0300

    creating neededs components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,7 @@
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="Footer-copy">
+      2020 PUGSTAGRAM
+    </div>
+  </div>
+</div>
---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div class="Profile">
+  <div class="Profile-content">
+    <div class="Profile-avatar">
+      <img src="https://avatars2.githubusercontent.com/u/18061371?s=460&u=15514a1b80843453d3e66c6a28c7ec672ae6b5a2&v=4" alt="" />
+    </div>
+    <div class="Profile-info">
+      <h2>esdraspavon</h2>
+      <span>Esdras Pavon</span>
+    </div>
+  </div>
+</div>
